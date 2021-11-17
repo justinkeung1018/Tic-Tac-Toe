6bf2dd5ebee328fb28bd7f4937117020eefe0ad3
justinkeung <92651832+justinkeung@users.noreply.github.com>
2021-11-17 15:48:18 +0000

    Pasted prompt into file

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,23 @@
-
+'''
+Write a program that creates tic-tac-toe. 
+Write a program that prints a grid like the following one:
+||  ||   ||  ||   ||  ||
+||  ||   ||  ||   ||  ||
+||  ||   ||  ||   ||  ||
+Two players play at turns and they have to choose a position in the grid.
+The position is chosen using the terminal and each player needs to provide a number,
+from 1 to 9. To each number there is a corresponding position in the grid, as follows:
+|| 1 ||   || 2 ||   || 3 ||
+|| 4 ||   || 5 ||   || 6 ||
+|| 7 ||   || 8 ||   || 9 ||
+For example, if player 1 (with X), at turn 1 chooses position 1, the grid will look like this:
+|| X ||   ||   ||   ||   ||
+||   ||   ||   ||   ||   ||
+||   ||   ||   ||   ||   ||
+Then, if player 2 (with O) chooses position 8, the grid will look like this:
+|| X ||   ||   ||   ||   ||
+||   ||   ||   ||   ||   ||
+||   ||   || O ||   ||   ||
+The game ends when one of the two players is able to allign their own marker on three different cells or when 
+it is impossible for any player to win
+'''
